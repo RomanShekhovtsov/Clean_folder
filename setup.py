027bdece6a824b4c397673aba86222db2bba2511
RomanShekhovtsov <roman13sheh@gmail.com>
2023-02-07 16:06:59 +0200

    Home work 2

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
-from setuptools import  setup, find_namespace_packages
-
+from setuptools import  setup
 setup (
-    name = "Cleaning"
-    version = "0.0.1"
-    description = "Cleaning folders"
-    author = "Shekhovtsov Roman"
-    author_email = roman13sheh@gmail.com
-    url = "https://github.com/RomanShekhovtsov"
-    license = "MIT"
+    name = "Cleaning",
+    version = "0.0.1",
+    description = "Cleaning folders",
+    author = "Shekhovtsov Roman",
+    author_email = roman13sheh@gmail.com,
+    url = "https://github.com/RomanShekhovtsov",
     classifiers = [
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: Windows 10 Home"]
-    packages = find_namespace_packages(),
-    data_files= [("Cleaning"),["Cleaning/clean.py"])],
+        "Operating System :: Windows 10 Home"],
+    data_files= [("Cleaning",["Cleaning/clean.py"])],
     include_package_data = True,
     entry_point = {"console_scripts": ["clean=Cleaning.clean:run"]}
 )
