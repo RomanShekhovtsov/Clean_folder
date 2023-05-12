864248c7ea92db4abbd313b818f5a6be61393a31
RomanShekhovtsov <roman13sheh@gmail.com>
2023-05-12 11:27:26 +0300

    new HM7

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
-from setuptools import  setup
-setup (
-    name = "Cleaning",
-    version = "0.0.1",
-    description = "Cleaning folders",
-    author = "Shekhovtsov Roman",
-    author_email = "roman13sheh@gmail.com",
-    url = "https://github.com/RomanShekhovtsov",
-    classifiers = [
+from setuptools import setup
+
+setup(
+    name="Cleaning",
+    version="0.0.1",
+    description="Cleaning folders",
+    author="Shekhovtsov Roman",
+    author_email="roman13sheh@gmail.com",
+    url="https://github.com/RomanShekhovtsov",
+    classifiers=[
         "Programming Language :: Python :: 3",
-        "Operating System :: Windows 10 Home"],
-    data_files= [("Cleaning",["Cleaning/clean.py"])],
-    include_package_data = True,
-    entry_point = {"console_scripts": ["clean=Cleaning.clean:run"]}
+        "Operating System :: Windows 10 Home"
+    ],
+    packages=["Cleaning"],  # Добавьте здесь название пакета
+    entry_points={
+        "console_scripts": [
+            "clean=Cleaning.clean:run"
+        ]
+    }
 )
